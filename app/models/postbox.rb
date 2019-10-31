381d370ec0bc08f6d0f1a54a40fbb5b456c52776
ShimeWataru <wawawatataru@gmail.com>
2019-10-31 18:35:11 +0900

    lint test の実行ファイルを作成

--- app/models/postbox.rb
+++ app/models/postbox.rb
@@ -2,8 +2,10 @@
 
 class Postbox < ApplicationRecord
   validates :uid,  presence: true
-  validates :title,  presence: true, length: { maximum: 40 }
-  validates :description,  presence: true, length: { maximum: 200 }
+  validates :title,  presence: { message: "が入力が必要です。" },
+            length: { maximum: 40, too_long: "#{:title}は最大#{count}文字まで入力できます。" }
+  validates :description,  presence: { message: "が入力が必要です。" },
+            length: { maximum: 200, too_long: "説明文は最大#{count}文字まで入力できます。" }
   validates :is_published,  inclusion: { in: [true, false] }
 
   has_many :posts
