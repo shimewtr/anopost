9e760903a8de859c4a80ec547ef85b376cb0f246
ShimeWataru <wawawatataru@gmail.com>
2019-11-01 22:47:59 +0900

    fix #23 postのバリデーションエラーを表示するように変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  validates :content,  presence: true, length: { maximum: 200 }
+  validates :content,  presence: true, length: { maximum: 200, too_long: "投稿は最大200文字まで入力できます。" }
 
   belongs_to :postbox
 end
--- app/models/postbox.rb
+++ app/models/postbox.rb
@@ -3,9 +3,9 @@
 class Postbox < ApplicationRecord
   validates :uid,  presence: true
   validates :title,  presence: { message: "が入力が必要です。" },
-            length: { maximum: 40, too_long: "#{:title}は最大#{count}文字まで入力できます。" }
+            length: { maximum: 40, too_long: "#{:title}は最大40文字まで入力できます。" }
   validates :description,  presence: { message: "が入力が必要です。" },
-            length: { maximum: 200, too_long: "説明文は最大#{count}文字まで入力できます。" }
+            length: { maximum: 200, too_long: "説明文は最大200文字まで入力できます。" }
   validates :is_published,  inclusion: { in: [true, false] }
 
   has_many :posts
