76d78533ffe47edf9b4a50a9cfe9d126b35e91eb
ShimeWataru <wawawatataru@gmail.com>
2019-11-04 22:20:57 +0900

    fix #28 投稿があったときスラックにも通知できる機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
     @postbox = Postbox.find_by(uid: params[:uid])
     @post = @postbox.posts.build(post_prams)
     if @post.save
+      notify_to_slack(@postbox, @post)
       redirect_to postbox_path(@postbox.uid), notice: "投稿しました"
     else
       redirect_to postbox_path(@postbox.uid), alert: @post.errors.full_messages_for(:content)
@@ -18,5 +19,22 @@ class PostsController < ApplicationController
   private
     def post_prams
       params.permit(:content)
+    end
+
+    def notify_to_slack(postbox, post)
+      if false
+        username = "AnoPost"
+        attachments = [{
+          title: postbox.title,
+          title_link: "#{request.scheme}://#{request.domain}:#{request.port}/#{postbox.uid}",
+          fallback: "comment body.",
+          author_name: "AnoPost",
+          author_icon: "https://raw.githubusercontent.com/wawawatataru/anopost/master/app/assets/images/android-chrome-256x256.png",
+          text: post.content,
+        }]
+        url = post.slack_webhook_url
+        notifier = Slack::Notifier.new url, username: username
+        notifier.ping "#{postbox.title}に投稿がありました。", attachments: attachments
+      end
     end
 end
