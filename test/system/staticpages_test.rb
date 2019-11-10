776c2da9bfab1eadfb58b1c5db79a672f649d857
ShimeWataru <wawawatataru@gmail.com>
2019-11-10 22:03:40 +0900

    fix #76 フッターリンクから遷移可能なページを修正

--- test/system/staticpages_test.rb
+++ test/system/staticpages_test.rb
@@ -3,18 +3,18 @@
 require "application_system_test_case"
 
 class StaticPagesTest < ApplicationSystemTestCase
-  test "このサイトについてページが正しく表示されるか" do
+  test "AnoPostとはページが正しく表示されるか" do
     visit static_pages_about_path
-    assert_text "このサイトについて"
+    assert_selector ".static-pages__title", text: "AnoPostとは"
   end
 
   test "免責事項ページが正しく表示されるか" do
     visit static_pages_discramer_path
-    assert_text "免責事項"
+    assert_selector ".static-pages__title", text: "免責事項"
   end
 
   test "Cookieポリシーページが正しく表示されるか" do
-    visit static_pages_about_path
-    assert_text "Cookieポリシー"
+    visit static_pages_privacy_path
+    assert_selector ".static-pages__title", text: "Cookieポリシー"
   end
 end
